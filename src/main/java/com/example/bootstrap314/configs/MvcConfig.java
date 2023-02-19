2b018eb421c2e34c7a9f08d085d5c7a2584019d4
Marina Panina <paninamar235@gmail.com>
2023-02-19 21:35:52 +1000

    done

--- src/main/java/com/example/bootstrap314/configs/MvcConfig.java
+++ src/main/java/com/example/bootstrap314/configs/MvcConfig.java
@@ -1,11 +1,10 @@
 package com.example.bootstrap314.configs;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebSecurity
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
--- src/main/java/com/example/bootstrap314/configs/WebSecurityConfig.java
+++ src/main/java/com/example/bootstrap314/configs/WebSecurityConfig.java
@@ -30,8 +30,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
+                .antMatchers("/user/**").hasAnyRole( "USER", "ADMIN")
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/**").hasAnyRole( "ADMIN","USER")
                 .antMatchers("/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
